7c408caa9772ea2aed97533331c321c56ec0edfa
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 04:38:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] => 4
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
